f97ffab909940ff54a001f817150a5bcf5885727
Abigail Ryan <abi.ryan91@gmail.com>
2024-04-11 19:35:36 +0000

    Add array of quiz questions to js file

---
+++ assets/js/script.js
@@ -0,0 +1,153 @@
+const questions = [
+    {
+        image : "assets/images/abu-simbel-temples-egypt.jpg",
+        question : "Representing the birth and coronation of King Rameses II, this is...",
+        answers : [
+            { A : "Mount Rushmore", correct : false},
+            { B : "Abu Simbel Temple", correct : true},
+            { C : "The Bayon Temple", correct : false},
+            { D : "Great Sphinx of Giza", correct : false},
+        ]
+    },
+    {
+        image : "assets/images/big-ben-london.jpg",
+        question : "This famous clock tower is...",
+        answers : [
+            { A : "The Rajabai Clock Tower ", correct : false},
+            { B : "The Prague Astronomical Clock", correct : false},
+            { C : "Big Ben", correct : true},
+            { D : "The Gros-Horloge", correct : false},
+        ]
+    },
+    {
+        image : "assets/images/burj-khalifa-dubai.jpg",
+        question : "The Burj Khalifa, the worlds tallest structure can be found in...",
+        answers : [
+            { A : "Dubai", correct : true},
+            { B : "Abu Dhabi", correct : false},
+            { C : "Dublin", correct : false},
+            { D : "Denver", correct : false},
+        ]
+    },
+    {
+        image : "assets/images/cliffs-of-moher-ireland.jpg",
+        question : "This famous cliff edge is...",
+        answers : [
+            { A : "White Cliffs of Dover", correct : false},
+            { B : "Slieve League", correct : false},
+            { C : "The Cliffs of Moher", correct : true},
+            { D : "Calvert Cliffs", correct : false},
+        ]
+    },
+    {
+        image : "assets/images/colosseum-rome.jpg",
+        question : "The largest ancient amphitheatre ever built is...",
+        answers : [
+            { A : "The Pula Arena", correct : false},
+            { B : "The Verona Arena", correct : false},
+            { C : "The Amphitheatre of El Jem", correct : false},
+            { D : "The Colosseum Rome", correct : true},
+        ]
+    },
+    {
+        image : "assets/images/death_valley_desert-cali.jpg",
+        question : "Thought to be the hottest place on Earth during summer, which desert is this?",
+        answers : [
+            { A : "Gobi Desert", correct : false},
+            { B : "Death Valley Desert", correct : true},
+            { C : "Sahara Desert", correct : false},
+            { D : "Kalahari Desert", correct : false},
+        ]
+    },
+    {
+        image : "assets/images/eiffel_tower_paris.jpg",
+        question : "Requiring 60 tons of paint every seven years to prevent it from rusting, The Eiffel Tower can be found in which city?",
+        answers : [
+            { A : "Perth", correct : false},
+            { B : "Paris", correct : true},
+            { C : "Phoenix", correct : false},
+            { D : "Padang", correct : false},
+        ]
+    },
+    {
+        image : "assets/images/grand-canyon-arizona.jpg",
+        question : "In 1909, which of these famous canyons was the site of a giant hoax?",
+        answers : [
+            { A : "The Grand Canyon of Yellowstone", correct : false},
+            { B : "Antelope Canyon", correct : false},
+            { C : "Kings Canyon", correct : false},
+            { D : "The Grand Canyon", correct : true},
+        ]
+    },
+    {
+        image : "assets/images/pyramids-of-giza-egypt.jpg",
+        question : "Which is not one of the Great Pyraminds of Giza?",
+        answers : [
+            { A : "Pyramid of Cheops", correct : false},
+            { B : "Pyramid of Khafre", correct : false},
+            { C : "Pyramid of Djoser", correct : true},
+            { D : "Pyramid of Menkaure", correct : false},
+        ]
+    },
+    {
+        image : "assets/images/sagrada-familia-barcelona.jpg",
+        question : "The largest unfinished Church in the world is...?",
+        answers : [
+            { A : "Sagrada Família", correct : true},
+            { B : "Notre Dame", correct : false},
+            { C : "St. Peter's Basilica", correct : false},
+            { D : "Cologne Cathedral", correct : false},
+        ]
+    },
+    {
+        image : "assets/images/statue-of-liberty-new-york.jpg",
+        question : "Lady Liberty is struck by lightning how many times every year?",
+        answers : [
+            { A : "Around 10 times", correct : false},
+            { B : "Around 200 times", correct : false},
+            { C : "Around 600 times", correct : true},
+            { D : "Around 25 times", correct : false},
+        ]
+    },
+    {
+        image : "assets/images/taj-mahal-india.jpg",
+        question : "Who did Mughal Emperor Shah Jahan build the Taj Mahal in memory of?",
+        answers : [
+            { A : "His mother", correct : false},
+            { B : "His favourite daughter", correct : false},
+            { C : "His favourite wife", correct : true},
+            { D : "His first wife", correct : false},
+        ]
+    },
+    {
+        image : "assets/images/the-great-wall-china.jpg",
+        question : "Great Wall of China was buuilt by which Dynasty?",
+        answers : [
+            { A : "Ming dynasty", correct : false},
+            { B : "Qin Dynasty,", correct : true},
+            { C : "Han Dynasty", correct : false},
+            { D : "Shang Dynasty", correct : false},
+        ]
+    },
+    {
+        image : "assets/images/the-louvre-paris.jpg",
+        question : "The Mona Lisa is the most famous art piece in The Louvre. What is the most valuable?",
+        answers : [
+            { A : "The Ain Ghazal Statue", correct : false},
+            { B : "The Mona Lisa,", correct : true},
+            { C : "Regent Diamond", correct : false},
+            { D : "Winged Victory of Samothrace", correct : false},
+        ]
+    },
+    {
+        image : "assets/images/the-sydney-opera-house-australia.jpg",
+        question : "in an international design competition held in 1956, how many designs were submitted for the Sydney Opera House?",
+        answers : [
+            { A : "8", correct : false},
+            { B : "75,", correct : false},
+            { C : "1456", correct : false},
+            { D : "233", correct : true},
+        ]
+    },
+]
+
