c1917c21a93e130711b15fe2c90f81941d7ad2ac
Abigail Ryan <abi.ryan91@gmail.com>
2024-04-22 18:49:40 +0000

    Remove function allowing user click anywhere to close game rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -195,14 +195,6 @@ span.onclick = function() {
   rulesModal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == rulesModal) {
-    rulesModal.style.display = "none";
-  }
-}
-
-
 
 // Global Variables
 const modal = document.getElementById("start-modal");
